b2c7f0f14c4ac5c513277c7918bc10cd3a4da074
shadyniks <shadynikslive@gmail.com>
2016-10-23 13:09:14 +0530

    migration change for pg

--- db/migrate/20160911192727_change_sort_date_type_from_date_time_to_int.rb
+++ db/migrate/20160911192727_change_sort_date_type_from_date_time_to_int.rb
@@ -1,5 +1,6 @@
 class ChangeSortDateTypeFromDateTimeToInt < ActiveRecord::Migration[5.0]
   def change
-  	change_column :articles, :sort_date, 'integer USING CAST(sort_date AS integer)'
+  	remove_column :articles, :sort_date
+  	add_column :articles, :sort_date, :integer
   end
 end
